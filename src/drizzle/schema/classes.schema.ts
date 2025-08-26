088268377e55a1e0c62f9a4cb8e40ace0202e4ba
coder7475 <robiulhossain7475@gmail.com>
2025-08-26 12:47:07 +0600

    feat: add user module with controller, service, and DTOs for user management

--- src/drizzle/schema/classes.schema.ts
+++ src/drizzle/schema/classes.schema.ts
@@ -1,4 +1,6 @@
 import { pgTable, uuid, varchar, timestamp } from 'drizzle-orm/pg-core';
+import { students } from './students.schema';
+import { relations } from 'drizzle-orm';
 
 // Classes table
 export const classes = pgTable('classes', {
@@ -7,3 +9,8 @@ export const classes = pgTable('classes', {
   section: varchar('section', { length: 255 }).notNull(),
   createdAt: timestamp('createdAt').defaultNow(),
 });
+
+// realationships
+export const classesRelations = relations(classes, ({ many }) => ({
+  students: many(students),
+}));
