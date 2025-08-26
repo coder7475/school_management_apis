bf7a19ee099e6b138722f8a5eff00b98dbfb3562
coder7475 <robiulhossain7475@gmail.com>
2025-08-26 19:47:24 +0600

    feat: enhance user retrieval with pagination and role-based access control in user controller and service

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { UserService } from './user.service';
+import { Roles } from 'src/auth/roles.decorator';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get()
-  findAll() {
-    return this.userService.findAll();
+  @Roles('admin')
+  findAll(
+    @Query('page') page: string = '1',
+    @Query('limit') limit: string = '10',
+  ) {
+    // Convert query params to numbers and pass to service
+    return this.userService.findAll(Number(page), Number(limit));
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,13 +2,35 @@ import { Inject, Injectable } from '@nestjs/common';
 
 import { DRIZZLE } from '../drizzle/drizzle.module';
 import type { DrizzleDB } from '../drizzle/types/drizzle';
+import { users } from 'src/drizzle/schema/users.schema';
 
 @Injectable()
 export class UserService {
   constructor(@Inject(DRIZZLE) private db: DrizzleDB) {}
 
-  async findAll() {
-    // return await this.db.select().from(users);
-    return await this.db.query.users.findMany();
+  async findAll(page: number = 1, limit: number = 10) {
+    const offset = (page - 1) * limit;
+    const result = await this.db.query.users.findMany({
+      limit,
+      offset,
+    });
+
+    // Get total count of users
+    const count = await this.db.$count(users);
+
+    const total = Number(count);
+    const metaData = {
+      total,
+      page,
+      limit,
+      totalPages: Math.ceil(total / limit),
+    };
+
+    return {
+      success: true,
+      message: 'Users list retrieved successfully',
+      data: result,
+      metaData,
+    };
   }
 }
