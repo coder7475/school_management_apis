8c52b8defd768135b0bd8e0d50b8fb46e197b5bb
coder7475 <robiulhossain7475@gmail.com>
2025-08-26 22:53:53 +0600

    feat: enhance class management by implementing role-based access control in ClassesController, updating class creation logic with Drizzle ORM, and adding validation to CreateClassDto

--- src/classes/classes.controller.ts
+++ src/classes/classes.controller.ts
@@ -1,34 +1,25 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, UseGuards } from '@nestjs/common';
 import { ClassesService } from './classes.service';
 import { CreateClassDto } from './dto/create-class.dto';
-import { UpdateClassDto } from './dto/update-class.dto';
+import { RolesGuard } from 'src/auth/roles.guard';
+import { JwtAuthGuard } from 'src/auth/jwt.guard';
+import { Roles } from 'src/auth/roles.decorator';
+import { Role } from 'src/types';
 
 @Controller('classes')
+@UseGuards(JwtAuthGuard, RolesGuard)
 export class ClassesController {
   constructor(private readonly classesService: ClassesService) {}
 
   @Post()
+  @Roles(Role.Admin)
   create(@Body() createClassDto: CreateClassDto) {
     return this.classesService.create(createClassDto);
   }
 
   @Get()
+  @Roles(Role.Admin)
   findAll() {
     return this.classesService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.classesService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateClassDto: UpdateClassDto) {
-    return this.classesService.update(+id, updateClassDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.classesService.remove(+id);
   }
 }
--- src/classes/classes.service.ts
+++ src/classes/classes.service.ts
@@ -1,26 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { CreateClassDto } from './dto/create-class.dto';
-import { UpdateClassDto } from './dto/update-class.dto';
+import { classes } from 'src/drizzle/schema/classes.schema';
+import type { DrizzleDB } from 'src/drizzle/types/drizzle';
+import { DRIZZLE } from 'src/drizzle/drizzle.module';
 
 @Injectable()
 export class ClassesService {
-  create(createClassDto: CreateClassDto) {
-    return 'This action adds a new class';
+  constructor(@Inject(DRIZZLE) private db: DrizzleDB) {}
+
+  // Create New Class
+  async create(createClassDto: CreateClassDto) {
+    const { name, section } = createClassDto;
+    const [classData] = await this.db
+      .insert(classes)
+      .values({ name, section })
+      .returning();
+
+    return {
+      success: true,
+      message: 'Student created successfully',
+      data: classData,
+    };
   }
 
   findAll() {
     return `This action returns all classes`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} class`;
-  }
-
-  update(id: number, updateClassDto: UpdateClassDto) {
-    return `This action updates a #${id} class`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} class`;
   }
 }
