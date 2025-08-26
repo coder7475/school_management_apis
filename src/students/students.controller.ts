fd1c825171577dff805e9071aa5bed71dfd0c12d
coder7475 <robiulhossain7475@gmail.com>
2025-08-26 22:27:52 +0600

    feat: enhance student management by implementing role-based access control, updating student creation logic with Drizzle ORM, and adding validation to CreateStudentDto

--- src/students/students.controller.ts
+++ src/students/students.controller.ts
@@ -1,34 +1,32 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, UseGuards } from '@nestjs/common';
 import { StudentsService } from './students.service';
 import { CreateStudentDto } from './dto/create-student.dto';
-import { UpdateStudentDto } from './dto/update-student.dto';
+
+import { Roles } from 'src/auth/roles.decorator';
+import { JwtAuthGuard } from 'src/auth/jwt.guard';
+import { RolesGuard } from 'src/auth/roles.guard';
+import { Role } from 'src/types';
 
 @Controller('students')
+@UseGuards(JwtAuthGuard, RolesGuard)
 export class StudentsController {
   constructor(private readonly studentsService: StudentsService) {}
 
   @Post()
+  @Roles(Role.Admin)
   create(@Body() createStudentDto: CreateStudentDto) {
     return this.studentsService.create(createStudentDto);
   }
 
   @Get()
+  @Roles(Role.Admin, Role.Teacher)
   findAll() {
     return this.studentsService.findAll();
   }
 
   @Get(':id')
+  @Roles(Role.Admin, Role.Teacher)
   findOne(@Param('id') id: string) {
     return this.studentsService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateStudentDto: UpdateStudentDto) {
-    return this.studentsService.update(+id, updateStudentDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.studentsService.remove(+id);
   }
 }
