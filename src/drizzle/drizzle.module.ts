df6b6173336ec5d4b80c20e8b6b09c20cd6dd247
coder7475 <robiulhossain7475@gmail.com>
2025-08-26 09:50:52 +0600

    feat: add @nestjs/config dependency and implement global DrizzleModule for database connection

--- src/drizzle/drizzle.module.ts
+++ src/drizzle/drizzle.module.ts
@@ -1,4 +1,27 @@
-import { Module } from '@nestjs/common';
+import { Module, Global } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Pool } from 'pg';
+import { drizzle, NodePgDatabase } from 'drizzle-orm/node-postgres';
+import * as schema from './schema';
+// unique identifier for drizzle provider
+export const DRIZZLE = Symbol('drizzle-connection');
 
-@Module({})
+@Global()
+@Module({
+  providers: [
+    {
+      provide: DRIZZLE,
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const db_url = configService.get<string>('DATABASE_URL');
+        const pool = new Pool({
+          connectionString: db_url,
+          ssl: true,
+        });
+        return drizzle(pool, { schema }) as NodePgDatabase<typeof schema>;
+      },
+    },
+  ],
+  exports: [DRIZZLE],
+})
 export class DrizzleModule {}
