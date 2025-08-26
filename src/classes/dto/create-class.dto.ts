0576efa9b7935cbcd65c4751de7a5b12a98a7708
coder7475 <robiulhossain7475@gmail.com>
2025-08-26 22:53:53 +0600

    feat: enhance class management by implementing role-based access control in ClassesController, updating class creation logic with Drizzle ORM, and adding validation to CreateClassDto

--- src/classes/dto/create-class.dto.ts
+++ src/classes/dto/create-class.dto.ts
@@ -1,1 +1,13 @@
-export class CreateClassDto {}
+import { IsString, IsNotEmpty, MinLength } from 'class-validator';
+
+export class CreateClassDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(2)
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(1)
+  section: string;
+}
