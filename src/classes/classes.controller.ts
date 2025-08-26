94710b35e63037eb3657fb94231fc439a7b681c2
coder7475 <robiulhossain7475@gmail.com>
2025-08-26 23:15:43 +0600

    feat: add student enrollment functionality in ClassesController and ClassesService, including validation and error handling for class and student existence

--- src/classes/classes.controller.ts
+++ src/classes/classes.controller.ts
@@ -1,20 +1,29 @@
-import { Controller, Get, Post, Body, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Body, UseGuards, Param } from '@nestjs/common';
 import { ClassesService } from './classes.service';
 import { CreateClassDto } from './dto/create-class.dto';
 import { RolesGuard } from 'src/auth/roles.guard';
 import { JwtAuthGuard } from 'src/auth/jwt.guard';
 import { Roles } from 'src/auth/roles.decorator';
 import { Role } from 'src/types';
+import { EnrollStudentDto } from './dto/enroll-student.dto';
 
 @Controller('classes')
 @UseGuards(JwtAuthGuard, RolesGuard)
 export class ClassesController {
   constructor(private readonly classesService: ClassesService) {}
 
+  // create class
   @Post()
   @Roles(Role.Admin)
   create(@Body() createClassDto: CreateClassDto) {
     return this.classesService.create(createClassDto);
+  }
+
+  // student enroll in an specific class
+  @Post(':id/enroll')
+  @Roles(Role.Admin, Role.Teacher)
+  async enroll(@Param('id') id: string, @Body() dto: EnrollStudentDto) {
+    return await this.classesService.enrollStudent(id, dto.studentId);
   }
 
   @Get()
